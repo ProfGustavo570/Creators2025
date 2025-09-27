cacb6f70bc2a6635d4068e4c721282e445e620b3
Gustavo Fernandes <gustavosergio2001@gmail.com>
2025-09-27 13:25:33 -0300

    MVP ready

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ import os
 import time
 
 import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 logging.basicConfig(
     handlers=[logging.FileHandler('app.log'), logging.StreamHandler()],
@@ -15,7 +12,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-Creators = {
+CREATORS = {
     'hainerd': {
         'INSTAGRAM': 'hain.nerd',
         'TIKTOK': '_hainerd_',
@@ -66,7 +63,7 @@ Creators = {
 
 def init_driver():
     try:
-        driver = uc.Chrome(headless=True, use_subprocess=False,)
+        driver = uc.Chrome(headless=True, use_subprocess=False)
         driver.implicitly_wait(10)
 
         return driver
@@ -77,52 +74,22 @@ def init_driver():
 
 def cycle_pages(driver):
     driver.get(f'https://instagram.com/virginia')
-
-    try:
-        # Espera até o botão de aceitar cookies aparecer
-        botao = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//*[contains(., 'Permitir todos os cookies')]"))
-        )
-        botao.click()
-    except:
-        print("Nenhum modal de cookies detectado")
-
+    time.sleep(10)
     driver.get(f'https://tiktok.com/@virginiafonseca')
-
-    try:
-        # Espera até o botão de aceitar cookies aparecer
-        botao = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//*[contains(., 'Permitir todos')]"))
-        )
-        botao.click()
-    except:
-        print("Nenhum modal de cookies detectado")
-
+    time.sleep(10)
     driver.get(f'https://www.youtube.com/channel/UCsaB2VrLZrtGbum6Eo0ClbA')
-
-    try:
-        # Espera até o botão de aceitar cookies aparecer
-        botao = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable(
-                (By.XPATH, "//*[contains(., 'Aceitar tudo')]"))
-        )
-        botao.click()
-    except:
-        print("Nenhum modal de cookies detectado")
 
 
 def get_screenshots():
     try:
         from datetime import datetime
 
-        date = datetime.now()
+        date = datetime.now().strftime("%m-%d")
 
         driver = init_driver()
         cycle_pages(driver)
 
-        for creator, redes in Creators.items():
+        for creator, redes in CREATORS.items():
             os.makedirs(f'./screenshots/{creator}', exist_ok=True)
 
             for rede, user in redes.items():
@@ -134,7 +101,8 @@ def get_screenshots():
                     time.sleep(10)
 
                     driver.save_screenshot(
-                        f'./screenshots/{creator}/{rede}-{date}.png')
+                        f'./screenshots/{creator}/{rede}_{date}.png'
+                    )
 
             logging.info(f'{creator} - OK')
 
